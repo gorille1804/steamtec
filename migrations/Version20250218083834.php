4916ec3e79547cbeb79f198d6d4ff2f6d29261cb
Tinah Fabrice <tinahfabrice.boss.etech@gmail.com>
2025-02-18 11:49:23 +0300

    fix idmachine et foreigne key user_id

--- migrations/Version20250218083834.php
+++ migrations/Version20250218083834.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250217145819 extends AbstractMigration
+final class Version20250218083834 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,7 @@ final class Version20250217145819 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE machine ADD user_id INT DEFAULT NULL, ADD numero_identification VARCHAR(255) NOT NULL, ADD nom VARCHAR(255) NOT NULL, ADD marque VARCHAR(255) NOT NULL, ADD temp_usage INT DEFAULT NULL, ADD seuil_maintenance INT NOT NULL');
+        $this->addSql('ALTER TABLE machine CHANGE id id VARCHAR(255) NOT NULL, CHANGE user_id user_id VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE machine ADD CONSTRAINT FK_1505DF84A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_1505DF84F5211ED ON machine (numero_identification)');
         $this->addSql('CREATE INDEX IDX_1505DF84A76ED395 ON machine (user_id)');
@@ -32,6 +32,6 @@ final class Version20250217145819 extends AbstractMigration
         $this->addSql('ALTER TABLE machine DROP FOREIGN KEY FK_1505DF84A76ED395');
         $this->addSql('DROP INDEX UNIQ_1505DF84F5211ED ON machine');
         $this->addSql('DROP INDEX IDX_1505DF84A76ED395 ON machine');
-        $this->addSql('ALTER TABLE machine DROP user_id, DROP numero_identification, DROP nom, DROP marque, DROP temp_usage, DROP seuil_maintenance');
+        $this->addSql('ALTER TABLE machine CHANGE id id INT AUTO_INCREMENT NOT NULL, CHANGE user_id user_id INT DEFAULT NULL');
     }
 }
