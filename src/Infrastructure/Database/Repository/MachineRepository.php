545dc181b30b5abb8c3abd97fa2bb858b7ca9508
Tinah Fabrice <tinahfabrice.boss.etech@gmail.com>
2025-02-18 15:53:12 +0300

    create machine

---
+++ src/Infrastructure/Database/Repository/MachineRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Infrastructure\Database\Repository;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Domain\Machine\Data\Model\Machine;
+use Domain\Machine\Data\ObjectValue\MachineId;
+use Domain\Machine\Gateway\MachineInterface;
+use Domain\Machine\Gateway\MachineRepositoryInterface;
+class MachineRepository extends ServiceEntityRepository implements MachineRepositoryInterface
+{
+    public function __construct(
+        ManagerRegistry $registry
+    )
+    {
+        parent::__construct($registry, Machine::class);
+    }
+    // public function getAll(): array
+    // {
+    //     return $this->findAll();
+    // }
+
+    // public function findByEmail(string $email): ?MachineInterface
+    // {
+    //     return $this->findOneBy(['email' => $email]);
+    // }
+
+    // public function findByid(MachineId $id): ?MachineInterface
+    // {
+    //     return $this->findOneBy(['id' => $id]);   
+    // }
+
+    public function save(Machine $machine): Machine
+    {
+        $em = $this->getEntityManager();
+        $em->persist($machine);
+        $em->flush();
+
+        return $machine;
+    }
+
+    // public function update(Machine $Machine): Machine
+    // {
+    //     $em = $this->getEntityManager();
+    //     $em->persist($Machine);
+    //     $em->flush();
+
+    //     return $Machine;
+    // }
+}
